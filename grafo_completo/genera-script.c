8640476185de4135f7c87783a0930d201d81f895
Nicolás Jares <nico.jar@gmail.com>
2018-01-21 13:00:03 -0300

    Promedio listo, sigue genera_D_G

--- grafo_completo/genera-script.c
+++ grafo_completo/genera-script.c
@@ -11,25 +11,38 @@ int main(){
 	size_t c_a_size = c_a_x * c_a_y * sizeof(float);
     int * A = malloc(A_size);
     float * c_a = malloc(c_a_size);
-	cargar_int(A,"data/input/matriz_A_2",A_x,A_y); // A=load(’data/input/matriz_A_2’);
-	cargar_float(c_a,"data/input/costo_aristas",c_a_x,c_a_y); // c_a=load(’data/input/costo_aristas’);
+    // A=load(’data/input/matriz_A_2’);
+	cargar_int(A,"data/input/matriz_A_2",A_x,A_y);
+	// c_a=load(’data/input/costo_aristas’);	
+	cargar_float(c_a,"data/input/costo_aristas",c_a_x,c_a_y);
+	// c_a=c_a(find(c_a));
+	// p=mean(c_a);
+	float p=0;
+	int n=0;
+	for (int i=0;i<c_a_x;i++){
+		for (int j=0;j<c_a_y;j++){
+			if (c_a[idx(i,j,c_a_y)]!=0.0){
+				p+=c_a[idx(i,j,c_a_y)];
+				n++;
+			}
+		}
+	}
+	p=p/n;
+	printf ("%f\n",p);
 	
-	for (int i=0;i<A_x;i++){
+/*	for (int i=0;i<A_x;i++){
 		for (int j=0;j<A_y;j++){
 			printf("%d ",A[idx(i,j,3)]);
 		}
 		printf("\n");
 	}
-	
 	getchar();
-	
 	for (int i=0;i<c_a_x;i++){
 		for (int j=0;j<c_a_y;j++){
 			printf("%d ",c_a[idx(i,j,3)]);
 		}
 		printf("\n");
-	}
-	
+	}*/
 	return 0;
 }
 
@@ -53,12 +66,10 @@ void cargar_float(float * A, char * ruta, int X_size, int Y_size){
 	archivo = fopen (ruta, "r");
 	for (int i=0;i<X_size;i++){
 		for (int j=0;j<Y_size;j++){
-			fscanf(archivo,"%d",&A[idx(i,j,Y_size)]);
+			fscanf(archivo,"%f",&A[idx(i,j,Y_size)]);
 		}
 	}
 	fclose(archivo);
 }
 
-/*c_a=c_a(find(c_a));
-p=mean(c_a);
-genera_D_G(’data/input/g_OD’,A,p,3,30);*/
+//genera_D_G(’data/input/g_OD’,A,p,3,30);
