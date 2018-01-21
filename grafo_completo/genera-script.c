ba8c34894e4691fa8454bbd7b5339ab2dd77b695
Nicolás Jares <nico.jar@gmail.com>
2018-01-21 00:31:32 -0300

    Duplique las funciones de cargar archivos. Sigue calcular el promedio y empezar con genera_D_G

--- grafo_completo/genera-script.c
+++ grafo_completo/genera-script.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 
 unsigned int idx(unsigned int i, unsigned int j, unsigned int stride);
-void cargar(int * A, char * ruta, int X_size, int Y_size);
+void cargar_int(int * A, char * ruta, int X_size, int Y_size);
+void cargar_float(float * A, char * ruta, int X_size, int Y_size);
 
 int main(){
-	unsigned int A_x=18012,A_y=3;
+	unsigned int A_x=18012,A_y=3,c_a_x=9006,c_a_y=1;
 	size_t A_size = A_x * A_y * sizeof(int);
+	size_t c_a_size = c_a_x * c_a_y * sizeof(float);
     int * A = malloc(A_size);
-	char ruta_A[] = "data/input/matriz_A_2";
-	cargar(A,"data/input/matriz_A_2",A_x,A_y); // A=load(’data/input/matriz_A_2’);
+    float * c_a = malloc(c_a_size);
+	cargar_int(A,"data/input/matriz_A_2",A_x,A_y); // A=load(’data/input/matriz_A_2’);
+	cargar_float(c_a,"data/input/costo_aristas",c_a_x,c_a_y); // c_a=load(’data/input/costo_aristas’);
 	
 	for (int i=0;i<A_x;i++){
 		for (int j=0;j<A_y;j++){
@@ -17,25 +20,45 @@ int main(){
 		}
 		printf("\n");
 	}
+	
+	getchar();
+	
+	for (int i=0;i<c_a_x;i++){
+		for (int j=0;j<c_a_y;j++){
+			printf("%d ",c_a[idx(i,j,3)]);
+		}
+		printf("\n");
+	}
+	
 	return 0;
 }
 
-unsigned int idx(unsigned int i, unsigned int j, unsigned int stride) {
-    return i * stride + j;
+unsigned int idx(unsigned int x, unsigned int y, unsigned int stride) {
+    return x * stride + y;
 }
 
-void cargar(int * A, char * ruta, int X_size, int Y_size){
+void cargar_int(int * A, char * ruta, int X_size, int Y_size){
 	FILE * archivo;
 	archivo = fopen (ruta, "r");
 	for (int i=0;i<X_size;i++){
 		for (int j=0;j<Y_size;j++){
-			fscanf(archivo,"%d",&A[idx(i,j,3)]);
+			fscanf(archivo,"%d",&A[idx(i,j,Y_size)]);
 		}
 	}
 	fclose(archivo);
 }
-	
-// c_a=load(’data/input/costo_aristas’);
+
+void cargar_float(float * A, char * ruta, int X_size, int Y_size){
+	FILE * archivo;
+	archivo = fopen (ruta, "r");
+	for (int i=0;i<X_size;i++){
+		for (int j=0;j<Y_size;j++){
+			fscanf(archivo,"%d",&A[idx(i,j,Y_size)]);
+		}
+	}
+	fclose(archivo);
+}
+
 /*c_a=c_a(find(c_a));
 p=mean(c_a);
 genera_D_G(’data/input/g_OD’,A,p,3,30);*/
