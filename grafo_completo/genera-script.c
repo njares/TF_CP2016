910138e391965a11009c9ee374f48ce79f497229
Nicolás Jares <nico.jar@gmail.com>
2018-02-02 02:50:53 -0300

    Funciones entrantes y salientes listas. Borradores de rutas y a_star empezados

--- grafo_completo/genera-script.c
+++ grafo_completo/genera-script.c
@@ -3,12 +3,17 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 unsigned int idx(unsigned int i, unsigned int j, unsigned int stride);
 void cargar_int(int * A, char * ruta, unsigned int X_size, unsigned int Y_size);
 void cargar_float(float * A, char * ruta, unsigned int X_size, unsigned int Y_size);
 void genera_D_G(char * ruta, int * A, float * c_a, float p,float t_max,unsigned int r_max);
-unsigned int rutas(int * Delta_aux, int i,int j,int * A, float p,float t_max,unsigned int r_max);
+unsigned int rutas(int * delta_aux, int i, int j,
+	int * A, float * nodos, float p, float t_max, unsigned int r_max, float * c_a);
+unsigned int a_star(int * y_aux, int i, int j, int * A, float * nodos, float * c_a);
+unsigned int salientes(int * i_1, int o, int * A);
+unsigned int entrantes(int * i_1, int d, int * A);
 
 unsigned int A_x=18012,A_y=3,c_a_x=9006,c_a_y=1;
 
@@ -76,28 +81,31 @@ void cargar_float(float * A, char * ruta, unsigned int X_size, unsigned int Y_si
 	fclose(archivo);
 }
 
-void genera_D_G(char * ruta, int * A,float * c_a,float p,float t_max,unsigned int r_max){
-	unsigned int g_x=2990,g_y=2,cant_rutas,i_ign=0;
+void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigned int r_max){
+	unsigned int g_x=2990,g_y=2,cant_rutas,i_ign=0,n_x=5237,n_y=2;
 	size_t g_size = g_x * g_y * sizeof(int);
 	size_t delta_size = c_a_x * r_max * 2 * sizeof(int);
+	size_t nodos_size = n_x * n_y * sizeof(float);
 	int * g_OD = malloc(g_size);
 	int * delta_aux = malloc(delta_size);
-	delta_t * delta_head = NULL; // Delta=[];
+	delta_t * delta_head = NULL; 
 	delta_head=malloc(sizeof(delta_t));
-	delta_t * current = delta_head;
-	int * ignorados = malloc(g_size); // ignorados=[];
+	delta_t * current = delta_head; // Delta=[];
+	int * ignorados = malloc(g_size); 
+	memset(ignorados, -1, g_size); // ignorados=[];
 	clock_t end,start;
 	float t=0; // t=0;
-	memset(ignorados, -1, g_size);
-	// g_OD=load(input1);
-	cargar_int(g_OD,ruta,g_x,g_y);
+	cargar_int(g_OD,ruta,g_x,g_y); // g_OD=load(input1);
+	float * nodos = malloc(nodos_size);
+	cargar_float(nodos,"data/input/nodos_amp",n_x,n_y);	// nodos=load("data/input/nodos_amp");
 	for (int i=0;i<g_x;i++){ // for i=1:rows(g_OD)
 		start=clock(); // tic
 		// printf("Procesando par %d de %d...\n",i,rows(g_OD))
 		printf("Procesando par %d de %d...\n",i+1,g_x); 
 		// y=rutas(g_OD(i,1),g_OD(i,2),A,p,t_max,r_max);
 		memset(delta_aux, 0, delta_size);
-		cant_rutas=rutas(delta_aux,g_OD[idx(i,0,g_y)],g_OD[idx(i,1,g_y)],A,p,t_max,r_max);
+		cant_rutas=rutas(delta_aux,g_OD[idx(i,0,g_y)],g_OD[idx(i,1,g_y)],
+			A,nodos,p,t_max,r_max,c_a);
 		// printf("Procesado par %d de %d\n",i,rows(g_OD))
 		printf("Procesado par %d de %d\n",i+1,g_x);
 		if (cant_rutas==0){ // if (length(y)==0)
@@ -139,7 +147,7 @@ void genera_D_G(char * ruta, int * A,float * c_a,float p,float t_max,unsigned in
 	for (int i=0;i<i_ign;i++){
 		fprintf(ignorados_file,"%d\n",ignorados[i]); // dlmwrite("data/output/ignorados",ignorados,'	')
 	}
-	while(delta_head != NULL){
+	while(delta_head->next != NULL){
 		current=delta_head;
 		delta_head=delta_head->next;
 		free(current->val);
@@ -150,50 +158,18 @@ void genera_D_G(char * ruta, int * A,float * c_a,float p,float t_max,unsigned in
 	free(delta_aux);
 } // endfunction
 
-unsigned int rutas(int * Delta_aux, int i,int j,int * A, float p,float t_max,unsigned int r_max){
-	sleep(1);
-	return 0;
-}
+unsigned int rutas(int * delta_aux, int i,int j,
+	int * A, float * nodos, float p, float t_max,unsigned int r_max, float * c_a)
+{
+	unsigned int largo_y_aux;
+	int y_aux[100];
+	largo_y_aux=a_star(y_aux,i,j,A,nodos,c_a); // y_aux=a_star(i,j,A); % busca el camino mas corto con A*
+	if (largo_y_aux==0){ // if (length(y_aux)==0)
+		return 0; // y=[];
+	} else { // else
+		return 1;
 
 /*
-# Uso:
-#
-#	y=rutas(i,j,A,p,t_max,r_max)
-#
-# Donde i, j y r_max deben ser enteros, A debe ser una matriz
-# de indices y valores de una matriz de incidencia de un 
-# grafo, y p y t_max deben ser reales.
-# Dados i y j indices de nodos de un grafo dado por la matriz
-# A de nxm, devuelve el arreglo y, de rxn, donde r es la 
-# cantidad de rutas encontradas entre i y j, y n es la 
-# cantidad de aristas del grafo. 'p' es el promedio de costo
-# de aristas del grafo, la %*ejecución*) de la segunda parte del 
-# algoritmo no excede t_max, y no busca mas de r_max rutas.
-# Primero realiza una %*búsqueda*) con un algoritmo 'A estrella',
-# y luego lleva adelante un algoritmo 'Busqueda en 
-# profundidad' para encontrar %*más*) rutas.
-# La cantidad %*máxima*) de nodos que puede tener un camino esta
-# en %*función*) de la longitud del camino %*óptimo*) encontrado con
-# 'A estrella' (se permiten aproximadamente un 25 % de nodos
-# %*más*)).
-# El promedio de costo de aristas de A es usado al llamar 
-# a dist_n para descartar caminos que se hayan alejado mucho
-# del destino.
-# Las rutas (filas) de y, son un arreglo que tiene un 1 en la
-# columna que se corresponde con cada arista que haya sido 
-# usada en esa ruta.
-# 'y' %*está*) en formato sparse
-# Si no encuentra %*ningún*) camino, devuelve un vector %*vacío*).
-#
-# NOTA: usa a_star.m, salientes.m, entrantes.m, dist_n.m y 
-# arista.m
-#
-function y=rutas(i,j,A,p,t_max,r_max)
-	nodos=load("data/input/nodos_amp");
-	y_aux=a_star(i,j,A); % busca el camino mas corto con A*
-	if (length(y_aux)==0)
-		y=[];
-	else
 		n=[];
 % calcula la distancia maxima de camino
 		n_max=floor(length(y_aux)*1.25)+1; 
@@ -316,6 +292,183 @@ function y=rutas(i,j,A,p,t_max,r_max)
 			endfor
 			y=[y;y_aux];
 		endfor
-	endif
-endfunction
 */
+
+	} // endif
+} // endfunction
+
+/*
+# NOTA: usa dist_n.m y arista.m
+*/
+
+unsigned int a_star(int * y_aux, int o, int d, int * A, float * nodos, float * c_a){
+	int i_1[30],i_1_size;
+/*
+	closed=[];
+*/
+	if (o>0) { // if(o>0) % si el grafo debe recorrerse 'mano'
+		// % si origen y destino %*están*) a menos de 300 m
+		if ( fabs(nodos[idx(o-1,0,2)]-nodos[idx(d-1,0,2)])*94870 +
+			 fabs(nodos[idx(o-1,1,2)]-nodos[idx(d-1,1,2)])*111180 < 300) { // if (sum(abs(nodos(o,:)-nodos(d,:)).*[94870 111180])<300) 
+			i_1_size=salientes(i_1,o,A); // i_1=(salientes(o,A))(1);
+			y_aux[0]=o;
+			y_aux[1]=i_1[0]; // y=[o i_1];
+			return 2;
+		} else { // else
+			return 0;
+/*
+			h_aux=sum(abs(nodos(o,:)-nodos(d,:)));
+			open=[o 0 h_aux];
+			came_from=zeros(rows(nodos),1);
+			while (rows(open)!=0)
+				[x i]=min(open(:,2)+open(:,3));
+% guardo el nodo mas barato en current
+				current=open(i,:);	
+				if (sum(abs(nodos(current(1),:)-nodos(d,:)).*[94870 111180])<300) 
+% si estoy a menos de 300 m del destino, termino
+					break;
+				endif
+% agrego el nodo actual a closed
+				closed=[closed;open(i,:)];
+				i_aux=setdiff([1:rows(open)],i);
+% saco el nodo actual de open
+				open=open(i_aux,:); 
+% busco los vecinos de current
+				i_1=salientes(current(1),A); 
+				for i=1:rows(i_1)
+% si ya esta en closed, sigo
+					if (length(find(closed(:,1)==i_1(i))))
+						continue;
+					endif
+					g_aux=current(2)+c_a(find(arista(current(1),i_1(i),A)));
+					h_aux=sum(abs(nodos(i_1(i),:)-nodos(d,:)));
+					if (length(find(open(:,1)==i_1(i)))) 
+% si ya %*está*) en open, lo dejo con el menor costo
+						i_aux=find(open(:,1)==i_1(i));
+						if(open(i_aux,2)>g_aux)
+							open(i_aux,2)=g_aux;
+							came_from(open(i_aux,1))=current(1);
+						endif
+					else	
+% si no %*está*) en open, lo agrego
+						open=[open;[i_1(i) g_aux h_aux]];
+						came_from(i_1(i))=current(1);
+					endif
+				endfor
+			endwhile
+			if (rows(open)==0)
+				y=[];
+			else
+% reconstruyo el camino
+				flag=current(1);
+				y=current(1);
+				while (flag!=o)
+					y=[came_from(flag);y];
+					flag=came_from(flag);
+				endwhile
+				y=y';
+			endif
+*/
+		} // endif
+	} else { // else % si el grafo debe recorrerse 'contramano'
+		o=-o; // o=-o;
+		d=-d; // d=-d;
+		if ( fabs(nodos[idx(o-1,0,2)]-nodos[idx(d-1,0,2)])*94870 +
+			 fabs(nodos[idx(o-1,1,2)]-nodos[idx(d-1,1,2)])*111180 < 300) { // if (sum(abs(nodos(o,:)-nodos(d,:)).*[94870 111180])<300) 
+			i_1_size=entrantes(i_1,d,A); // i_1=(entrantes(d,A))(1);
+			y_aux[0]=i_1[0];
+			y_aux[1]=d; // y=[i_1 d];
+			return 2;
+		} else { // else
+			return 0;
+/*
+			h_aux=sum(abs(nodos(o,:)-nodos(d,:)));
+			open=[d 0 h_aux];
+			came_from=zeros(rows(nodos),1);
+			while (rows(open)!=0)
+				[x i]=min(open(:,2)+open(:,3));
+				current=open(i,:);
+				if (sum(abs(nodos(current(1),:)-nodos(o,:)).*[94870 111180])<300) 
+					break;
+				endif
+				closed=[closed;open(i,:)];
+				i_aux=setdiff([1:rows(open)],i);
+				open=open(i_aux,:);
+				i_1=entrantes(current(1),A);
+				for i=1:rows(i_1)
+					if (length(find(closed(:,1)==i_1(i))))
+						continue;
+					endif
+					g_aux=current(2)+c_a(find(arista(i_1(i),current(1),A)));
+					h_aux=sum(abs(nodos(i_1(i),:)-nodos(o,:)));
+					if (length(find(open(:,1)==i_1(i)))) 
+						i_aux=find(open(:,1)==i_1(i));
+						if(open(i_aux,2)>g_aux)
+							open(i_aux,2)=g_aux;
+							came_from(open(i_aux,1))=current(1);
+						endif	
+					else
+						open=[open;[i_1(i) g_aux h_aux]];
+						came_from(i_1(i))=current(1);
+					endif
+				endfor
+			endwhile
+			if (rows(open)==0)
+				y=[];
+			else
+				flag=current(1);
+				y=current(1);
+				while (flag!=d)
+					y=[y;came_from(flag)];
+					flag=came_from(flag);
+				endwhile
+				y=y';
+			endif
+*/			
+		} // endif
+	} // endif
+} // endfunction
+
+/*
+# NOTA: usa arista.m
+*/
+
+unsigned int salientes(int * i_1, int o, int * A){
+	int i_aux[30],size=0,k=0;
+	for (int i=0;i<A_x;i++){
+		if (A[idx(i,1,A_y)]==o){
+			i_aux[size]=A[idx(i,0,A_y)];
+			size++;
+		}
+	}
+	for (int i=0;i<A_x;i++){
+		for (int j=0;j<size;j++){
+			if ( A[idx(i,0,A_y)]==i_aux[j] && 
+				 A[idx(i,1,A_y)]!=o) {
+				i_1[k]=A[idx(i,1,A_y)];
+				k++;
+			}
+		}
+	}
+	return size;
+}
+
+unsigned int entrantes(int * i_1, int d, int * A){
+	int i_aux[30],size=0,k=0;
+	for (int i=0;i<A_x;i++){
+		if (A[idx(i,1,A_y)]==d){
+			i_aux[size]=A[idx(i,0,A_y)];
+			size++;
+		}
+	}
+	for (int i=0;i<A_x;i++){
+		for (int j=0;j<size;j++){
+			if ( A[idx(i,0,A_y)]==i_aux[j] && 
+				 A[idx(i,1,A_y)]!=d) {
+				i_1[k]=A[idx(i,1,A_y)];
+				k++;
+			}
+		}
+	}
+	return size;
+}
