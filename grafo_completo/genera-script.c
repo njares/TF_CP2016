339cfdcf8a8ffd6198e24940ca0274dd13a866d7
Nicolás Jares <nico.jar@gmail.com>
2018-07-22 23:21:48 -0300

    funcion rutas debugeada, genera-script parece listo, genera las matrices Delta y Gamma aparentemente sin problemas, falta chequear correctitud en octave

--- grafo_completo/genera-script.c
+++ grafo_completo/genera-script.c
@@ -49,8 +49,8 @@ int main(){
 		}
 	}
 	p=p/n;
-	unsigned int r_max=30;
-	float t_max=3;
+	unsigned int r_max=5;
+	float t_max=0.3;
 	//genera_D_G(’data/input/g_OD’,A,p,3,30);
 	genera_D_G("data/input/g_OD",A,c_a,p,t_max,r_max);
 	free(A);
@@ -112,7 +112,7 @@ void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigne
 		// printf("Procesado par %d de %d\n",i,rows(g_OD))
 		printf("Procesado par %d de %d\n",i+1,g_x);
 		if (cant_rutas==0){ // if (length(y)==0)
-			ignorados[i_ign]=i; // ignorados=[ignorados;i];
+			ignorados[i_ign]=i+1; // ignorados=[ignorados;i];
 			i_ign++;
 			// printf("No se encontraron rutas para el par %d, agregado a la lista de pares ignorados\n",i)
 			printf("No se encontraron rutas para el par %d, agregado a la lista de pares ignorados\n",i+1);
@@ -120,7 +120,7 @@ void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigne
 			current->val=malloc(delta_size);
 			memcpy(current->val,delta_aux,delta_size); // Delta=[Delta;y];
 			current->largo=cant_rutas;
-			current->val=realloc(current->val,current->largo*sizeof(int));
+			current->val=realloc(current->val,current->largo*2*sizeof(int));
 			current->next=malloc(sizeof(delta_t));
 			current->next->next=NULL;
 			current=current->next;
@@ -128,7 +128,7 @@ void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigne
 		end=clock();
 		t+= (float) (end-start)/CLOCKS_PER_SEC; // t=t+toc;
 		// printf("Transcurrieron %d segundos. Tiempo restante estimado: %d segundos\n",t,t*(rows(g_OD)-i)/i)
-		printf("Transcurrieron %f segundos. Tiempo restante estimado: %f segundos\n",t,t*(g_x-i+1)/(i+1));
+		printf("Transcurrieron %f segundos. Tiempo restante estimado: %f segundos\n",t,t*(g_x-i)/(i+1));
 	} // endfor
 	current=delta_head;
 	FILE * delta_file;
@@ -137,16 +137,15 @@ void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigne
 	delta_file=fopen("data/output/Delta","w+");
 	gamma_file=fopen("data/output/Gamma","w+");
 	ignorados_file=fopen("data/output/ignorados","w+");
-/*
+	int fila_so_far=0;
 	while(current->val != NULL){
 		for (int j=0;j<current->largo;j++){
-			fprintf(delta_file,"%d ",current->val[j]); // dlmwrite("data/output/Delta",[i j x],'	')
+			fprintf(delta_file,"%d %d\n",current->val[idx(j,0,2)]+fila_so_far,current->val[idx(j,1,2)]); // dlmwrite("data/output/Delta",[i j x],'	')
 		}
-		fprintf(gamma_file,"%d\n",current->largo); // dlmwrite("data/output/Gamma",[i j x],'	')
-		fprintf(delta_file,"\n",current->val[j]);
+		fprintf(gamma_file,"%d\n",current->val[idx(current->largo-1,0,2)]); // dlmwrite("data/output/Gamma",[i j x],'	')
+		fila_so_far+=current->val[idx(current->largo-1,0,2)];
 		current=current->next;
 	}
-*/
 	for (int i=0;i<i_ign;i++){
 		fprintf(ignorados_file,"%d\n",ignorados[i]); // dlmwrite("data/output/ignorados",ignorados,'	')
 	}
@@ -164,9 +163,9 @@ void genera_D_G(char * ruta, int * A, float * c_a, float p, float t_max, unsigne
 unsigned int rutas(int * delta_aux, int i,int j,
 	int * A, float * nodos, float p, float t_max,unsigned int r_max, float * c_a)
 {
-	unsigned int largo_y_aux,n_max,y_aux[100],i_n=0,i_1[30],i_1_size,i_s=0,current;
+	unsigned int largo_y_aux,n_max,y_aux[100],i_n=0,i_1[30],i_1_size,i_s=0;
 	int i_aux;
-	float t;
+	float t=0,start,end;
 	largo_y_aux=a_star(y_aux,i,j,A,nodos,c_a); // y_aux=a_star(i,j,A); % busca el camino mas corto con A*
 	if (largo_y_aux==0){ // if (length(y_aux)==0)
 		return 0; // y=[];
@@ -178,6 +177,7 @@ unsigned int rutas(int * delta_aux, int i,int j,
 		size_t size_s=n_max*n_max*30*sizeof(unsigned int);
 		int * n=malloc(size_n); // n=[];
 		int * s=malloc(size_s); // s=[];
+		int current[n_max];
 		memset(n,-1,size_n);
 		memset(s,-1,size_s);
 		memcpy(n,y_aux,largo_y_aux*sizeof(unsigned int)); // n=[n;y_aux];
@@ -188,18 +188,18 @@ unsigned int rutas(int * delta_aux, int i,int j,
 			// k_y=length(find(y_aux));
 			for (int k=0;k<largo_y_aux-1;k++) { // for k=1:k_y-1
 				// % encuentro salientes para nodos salvo el %*último*)
-				i_1_size=salientes(i_1,y_aux[k],A); // i_1=salientes(y_aux(k_y-k),A);
+				i_1_size=salientes(i_1,y_aux[k]+1,A); // i_1=salientes(y_aux(k_y-k),A);
 				for (int l=0;l<i_1_size;l++) {
 					// [c i1 i2]=intersect(i_1,y_aux(k_y-k+1));
 					// % saco el que ya %*había*) sido 'encontrado'
-					if (i_1[l]!=y_aux[k+1]) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
+					if (i_1[l]!=y_aux[k+1]+1) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
 						// % calculo los caminos para agregar a la pila
 						for (int m=0;m<k+1;m++) { 
 							s[idx(i_s,m,n_max)]=y_aux[m]; // v=y_aux(1:(k_y-k));
 							// v(rows(v),length(y_aux))=0;
 							// v=repmat(v,length(i_1),1);
 						}
-						s[idx(i_s,k+1,n_max)]=i_1[l]; // // v(:,k_y-k+1)=i_1; 
+						s[idx(i_s,k+1,n_max)]=i_1[l]-1; // // v(:,k_y-k+1)=i_1; 
 						// % agrego los nuevos caminos a la pila
 						i_s++; // s=[v;s]; 
 					}
@@ -210,17 +210,20 @@ unsigned int rutas(int * delta_aux, int i,int j,
 			l=0;
 			i_max=0;
 			i_min=n_max+1;
-			tic
 */
+			start=clock(); // tic
+			end=clock();
+			t= (float) (end-start)/CLOCKS_PER_SEC;
 			while (i_s!=0 && t<t_max && i_n < r_max) { // while (rows(s)!=0 && toc<t_max && rows(n)<r_max)
 				// % pongo el ultimo elemento de la pila en v
-				current=i_s; // v=s(rows(s),:);	
+				memcpy(&current[0],&s[idx(i_s-1,0,n_max)],n_max*sizeof(int));// v=s(rows(s),:);	
 				// % saco el ultimo elemento de la pila
+				memset(&s[idx(i_s-1,0,n_max)],-1,n_max*sizeof(int));
 				i_s--; // s=s([1:rows(s)-1],:);
 				// % guardo la posicion del %*último*) elemento de v
 				i_aux=-1;
 				for (int k=0;k<n_max;k++) {
-					if (s[idx(current,k,n_max)]==-1) {
+					if (current[k]==-1) {
 						i_aux=k-1; // i_aux=max(find(v));
 						k=n_max;
 					}
@@ -228,10 +231,11 @@ unsigned int rutas(int * delta_aux, int i,int j,
 				if (i_aux==-1) {
 					i_aux=n_max-1;
 				}
-				if (fabs(nodos[idx(s[idx(current,i_aux,n_max)],0,n_y)]-nodos[idx(j-1,0,n_y)])*94870 +
-					fabs(nodos[idx(s[idx(current,i_aux,n_max)],1,n_y)]-nodos[idx(j-1,1,n_y)])*111180 < 300) { // if (sum(abs(nodos(v(i_aux),:)-nodos(j,:)).*[94870 111180])<300) 
+				if (fabs(nodos[idx(current[i_aux],0,n_y)]-nodos[idx(j-1,0,n_y)])*94870 +
+					fabs(nodos[idx(current[i_aux],1,n_y)]-nodos[idx(j-1,1,n_y)])*111180 < 300) { // if (sum(abs(nodos(v(i_aux),:)-nodos(j,:)).*[94870 111180])<300) 
 					// % si estoy a menos de 300 m del destino, agrego el camino a n
-					memcpy(&n[idx(i_n,0,n_max)],&s[idx(current,0,n_max)],n_max*sizeof(int)); // n=[n;v];
+					memcpy(&n[idx(i_n,0,n_max)],&current[0],n_max*sizeof(int)); // n=[n;v];
+					i_n++;
 /*
 					k=k+1;
 					i_max=max(i_max,i_aux);
@@ -244,7 +248,7 @@ unsigned int rutas(int * delta_aux, int i,int j,
 					k=k+1;
 */					
 					continue; // continue;
-				} else if (n_max-i_aux<dist_n(s[idx(current,i_aux,n_max)],j,p,nodos)) { // elseif (n_max-i_aux<dist_n(v(i_aux),j,p,nodos))
+				} else if (n_max-i_aux<dist_n(current[i_aux]+1,j,p,nodos)) { // elseif (n_max-i_aux<dist_n(v(i_aux),j,p,nodos))
 					// % si no llegue, y me %*alejé*) demasiado, descarto este camino
 /*
 					l=l+2**(n_max-i_aux);
@@ -254,7 +258,7 @@ unsigned int rutas(int * delta_aux, int i,int j,
 					// % si no llegue, y no alcance la cantidad %*máxima*) de nodos, 
 					// % agrego nuevos nodos
 					// % averiguo los adyacentes al %*último*) elemento de v
-					i_1_size=salientes(i_1,s[idx(current,i_aux,n_max)],A); // i_1=salientes(v(i_aux),A);
+					i_1_size=salientes(i_1,current[i_aux]+1,A); // i_1=salientes(v(i_aux),A);
 					// % chequeo si se repite con alguno de los ya visitados
 					int flag;
 					for (int k=0;k<i_1_size;k++) {
@@ -262,7 +266,7 @@ unsigned int rutas(int * delta_aux, int i,int j,
 						// % saco los repetidos
 						flag=0;
 						for (int l=0;l<i_aux && !flag;l++) {
-							if (i_1[k]==s[idx(current,l,n_max)]) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
+							if (i_1[k]==current[l]+1) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
 								flag=1;
 							}
 						}
@@ -271,32 +275,34 @@ unsigned int rutas(int * delta_aux, int i,int j,
 						} else {
 							// % calculo los nuevos caminos para agregar a la pila
 							for (int l=0;l<i_aux+1;l++) {
-								s[idx(i_s,l,n_max)]=s[idx(current,l,n_max)];
+								s[idx(i_s,l,n_max)]=current[l];
 								// v=repmat(v,length(i_1),1);
 							}
-							s[idx(i_s,i_aux+1,n_max)]=i_1[k]; // // v(:,i_aux+1)=i_1;
+							s[idx(i_s,i_aux+1,n_max)]=i_1[k]-1; // // v(:,i_aux+1)=i_1;
 							// % agrego los nuevos caminos a la pila
 							i_s++; // s=[s;v];
 						}
 					}
 				} // endif
+			end=clock();
+			t= (float) (end-start)/CLOCKS_PER_SEC;
 			} // endwhile
 		}else{ // else % Lo mismo, pero 'contramano'
 			i=-i; // i=-i;
 			j=-j; // j=-j;
 			// k_y=length(find(y_aux));
 			for (int k=largo_y_aux-1;k>0;k--) { // for k=2:k_y
-				i_1_size=entrantes(i_1,y_aux[k],A); // i_1=entrantes(y_aux(k),A);
+				i_1_size=entrantes(i_1,y_aux[k]+1,A); // i_1=entrantes(y_aux(k),A);
 				for (int l=0;l<i_1_size;l++) {
 					// [c i1 i2]=intersect(i_1,y_aux(k-1));
-					if (i_1[l]!=y_aux[k-1]) { // i_1=i_1(setdiff(1:rows(i_1),i2));
+					if (i_1[l]!=y_aux[k-1]+1) { // i_1=i_1(setdiff(1:rows(i_1),i2));
 						for (int m=1;m<largo_y_aux-k+1;m++) {
 							s[idx(i_s,m,n_max)]=y_aux[k+m-1]; // v=y_aux(k:k_y);
 							// v(rows(v),length(y_aux)-1)=0;
 							// v=[0,v];
 							// v=repmat(v,length(i_1),1);
 						}
-						s[idx(i_s,0,n_max)]=i_1[l]; // v(:,1)=i_1;
+						s[idx(i_s,0,n_max)]=i_1[l]-1; // v(:,1)=i_1;
 						i_s++; // s=[v;s];
 					}
 				}
@@ -306,14 +312,17 @@ unsigned int rutas(int * delta_aux, int i,int j,
 			l=0;
 			i_max=0;
 			i_min=n_max+1;
-			tic
 */
+			start=clock(); // tic
+			end=clock();
+			t= (float) (end-start)/CLOCKS_PER_SEC;
 			while (i_s!=0 && t<t_max && i_n < r_max) { // while (rows(s)!=0 && toc<t_max && rows(n)<r_max)
-				current=i_s; // v=s(rows(s),:);
+				memcpy(&current[0],&s[idx(i_s-1,0,n_max)],n_max*sizeof(int));// v=s(rows(s),:);	
+				memset(&s[idx(i_s-1,0,n_max)],-1,n_max*sizeof(int));
 				i_s--; // s=s([1:rows(s)-1],:);
 				i_aux=-1;
 				for (int k=0;k<n_max;k++) {
-					if (s[idx(current,k,n_max)]==-1) {
+					if (current[k]==-1) {
 						i_aux=k-1; // i_aux=max(find(v));
 						k=n_max;
 					}
@@ -321,9 +330,10 @@ unsigned int rutas(int * delta_aux, int i,int j,
 				if (i_aux==-1) {
 					i_aux=n_max-1;
 				}
-				if (fabs(nodos[idx(s[idx(current,0,n_max)],0,n_y)]-nodos[idx(i-1,0,n_y)])*94870 +
-					fabs(nodos[idx(s[idx(current,0,n_max)],1,n_y)]-nodos[idx(i-1,1,n_y)])*111180 < 300) { // if (sum(abs(nodos(v(1),:)-nodos(i,:)).*[94870 111180])<300)
-					memcpy(&n[idx(i_n,0,n_max)],&s[idx(current,0,n_max)],n_max*sizeof(int)); // n=[n;v];
+				if (fabs(nodos[idx(current[0],0,n_y)]-nodos[idx(i-1,0,n_y)])*94870 +
+					fabs(nodos[idx(current[0],1,n_y)]-nodos[idx(i-1,1,n_y)])*111180 < 300) { // if (sum(abs(nodos(v(1),:)-nodos(i,:)).*[94870 111180])<300)
+					memcpy(&n[idx(i_n,0,n_max)],&current[0],n_max*sizeof(int)); // n=[n;v];
+					i_n++;
 /*
 					k=k+1;
 					i_max=max(i_max,i_aux);
@@ -334,60 +344,63 @@ unsigned int rutas(int * delta_aux, int i,int j,
 					k=k+1;
 */					
 					continue; // continue;
-				} else if (n_max-i_aux<dist_n(s[idx(current,0,n_max)],i,p,nodos)) { // elseif (n_max-i_aux<dist_n(v(1),i,p,nodos))
+				} else if (n_max-i_aux<dist_n(current[0]+1,i,p,nodos)) { // elseif (n_max-i_aux<dist_n(v(1),i,p,nodos))
 /*
 					l=l+2**(n_max-i_aux);
 */
 					continue; // continue;
 				} else { // else
-					i_1_size=entrantes(i_1,s[idx(current,0,n_max)],A); // i_1=entrantes(v(1),A); 
+					i_1_size=entrantes(i_1,current[0]+1,A); // i_1=entrantes(v(1),A); 
 					int flag;
 					for (int k=0;k<i_1_size;k++) {
 						// [c i1 i2]=intersect(v,i_1);
 						flag=0;
 						for (int l=0;l<i_aux && !flag;l++) {
-							if (i_1[k]==s[idx(current,l,n_max)]) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
+							if (i_1[k]==current[l]+1) { // i_1=i_1(setdiff(1:rows(i_1),i2)); 
 								flag=1;
 							}
 						}
 						if (flag) {
 							continue;
 						} else {
-							for (int l=1;l<i_aux+2;l++){
-								s[idx(i_s,l,n_max)]=s[idx(current,l-1,n_max)];
+							for (int l=i_aux+1;l>0;l--){
+								s[idx(i_s,l,n_max)]=current[l-1];
 								// v=[0 v(1:(length(v)-1))];
 								// v=repmat(v,length(i_1),1);
 							}
-							s[i_s,0,n_max]=i_1[k]; // v(:,1)=i_1;
+							s[idx(i_s,0,n_max)]=i_1[k]-1; // v(:,1)=i_1;
 							i_s++; // s=[s;v];
 						}
 					}
 				} // endif
+			end=clock();
+			t= (float) (end-start)/CLOCKS_PER_SEC;
 			} // endwhile			
 		} // endif
-
-/*
-		y=[];
-% reconstruyo los caminos para devolverlos
-		for k=1:rows(n)
-			y_aux=sparse([],[],[],1,max(A(:,1))); 
-			for l=1:max(find(n(k,:)))-1
-				y_aux=y_aux+arista(n(k,l),n(k,l+1),A);
-			endfor
-			y=[y;y_aux];
-		endfor
-*/
-
-		return 1;
-	free(n);
-	free(s);
+		// % reconstruyo los caminos para devolverlos
+		int i_delta=0; // y=[];
+		for(int k=0;k<i_n;k++){ // for k=1:rows(n)
+			// y_aux=sparse([],[],[],1,max(A(:,1))); 
+			for (int l=0;l<n_max;l++){ // for l=1:max(find(n(k,:)))-1
+				// y_aux=y_aux+arista(n(k,l),n(k,l+1),A);
+				if (n[idx(k,l,n_max)]!=-1){
+					delta_aux[idx(i_delta,0,2)]=k+1;
+					delta_aux[idx(i_delta,1,2)]=n[idx(k,l,n_max)]+1;
+					i_delta++;
+				}
+			} // endfor
+			// y=[y;y_aux];
+		} // endfor
+		free(n);
+		free(s);
+		return i_delta;
 	} // endif
 } // endfunction
 
 unsigned int a_star(unsigned int * y_aux, int o, int d, int * A, float * nodos, float * c_a){
 	size_t n_size = n_x * 4 * sizeof(float);
 	unsigned int i_1[30],i_1_size,open_size=0,i_star,flag,ruta_reves[100],i_r=0;
-	float * n_star = malloc(n_size); // [conjunto costo_acum costo_falt came_from]
+	float * n_star = malloc(n_size); // [conjunto costo_acum costo_falt came_from], conjunto=1 : abierto, conjunto=2 : cerrado
 	float h_aux,current[4],g_aux;
 	for (int i=0;i<n_x;i++) {
 		n_star[idx(i,0,4)]=-1;
